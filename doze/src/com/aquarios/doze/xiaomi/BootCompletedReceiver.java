c2872b0ba835797d61983699f802f0d2ce54c0e6
rc420head <dennislebhardt@yahoo.com>
2017-09-02 10:20:27 -0500

    this is the age of AquaiOS

--- doze/src/com/aquarios/doze/xiaomi/BootCompletedReceiver.java
+++ doze/src/com/aquarios/doze/xiaomi/BootCompletedReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (c) 2017 The aquariosRoms Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.doze;
+package com.aquarios.doze.xiaomi;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -28,10 +28,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (Utils.isDozeEnabled(context) && Utils.sensorsEnabled(context)) {
-            if (DEBUG) Log.d(TAG, "Starting service");
-            Utils.startService(context);
-        }
+        if (DEBUG) Log.d(TAG, "Starting service");
+        context.startService(new Intent(context, XiaomiDozeService.class));
     }
 
 }
